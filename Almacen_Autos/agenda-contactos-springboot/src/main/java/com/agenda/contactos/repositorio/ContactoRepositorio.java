32151c2f80e522a9aae6f0e32a4027f899926f98
lizbeth sanchez <114268539+LizbethSanchezCajavilca@users.noreply.github.com>
2022-12-19 18:50:22 -0500

    Delete Almacen_Autos/agenda-contactos-springboot directory

--- Almacen_Autos/agenda-contactos-springboot/src/main/java/com/agenda/contactos/repositorio/ContactoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.agenda.contactos.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agenda.contactos.modelo.Contacto;
-
-public interface ContactoRepositorio extends JpaRepository<Contacto, Integer>{
-
-}
